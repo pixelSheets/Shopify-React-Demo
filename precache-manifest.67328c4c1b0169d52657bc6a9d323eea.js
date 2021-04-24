6b94a4d128599627fa7231fb0d1a57ba4074876c
sheetWeaver <admin@pixelsheets.com>
2021-04-24 11:53:02 -0700

    Updates

---
+++ precache-manifest.67328c4c1b0169d52657bc6a9d323eea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3989be55eb03bfb2f836dc867f9939d9",
+    "url": "/Shopify-React-Demo/index.html"
+  },
+  {
+    "revision": "6b156712ac6f64809181",
+    "url": "/Shopify-React-Demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4a63750ed92f12ee5e4e",
+    "url": "/Shopify-React-Demo/static/js/2.270d7ba2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Shopify-React-Demo/static/js/2.270d7ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b156712ac6f64809181",
+    "url": "/Shopify-React-Demo/static/js/main.bd63cd73.chunk.js"
+  },
+  {
+    "revision": "0ee1a38c86f6e133e3c8",
+    "url": "/Shopify-React-Demo/static/js/runtime-main.2d41d711.js"
+  }
+]);
